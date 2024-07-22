86ed9e5290e3e5da66152eb608cbff5abcae2889
Vladislav <danilevicvladislav@gmail.com>
2024-07-22 16:58:37 +0300

    The update fixed a lot of bugs and the bot itself started working properly

--- Telegram_bot_quiz.py
+++ Telegram_bot_quiz.py
@@ -2,67 +2,72 @@ import telebot
 from telebot import types
 import random
 
-TOKEN_TELEGRAM = 'XXX'
+TOKEN_TELEGRAM = 'xxx'
 bot = telebot.TeleBot(TOKEN_TELEGRAM)
-Total_PROGRESS_of_RESPONCES = {}
+Total_PROGRESS_of_RESPONSES = {}
 
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
+def message_start(message):
+    bot.send_message(message.chat.id,
+                     'Команды для управления ботом:\n/start - начать сначала.\n/start_game - начать игру.')
+
+
+@bot.message_handler(commands=['start_game'])
+def start_game_message(message):
     chat_id = message.chat.id
-    Total_PROGRESS_of_RESPONCES[chat_id] = 0
-    bot.send_message(message.chat.id, 'Добро пожаловать в игру угадай число')
-    markup = types.ReplyKeyboardMarkup()
+    Total_PROGRESS_of_RESPONSES[chat_id] = 0
+    bot.send_message(chat_id, 'Добро пожаловать в игру "Угадай число"')
+    markup = types.ReplyKeyboardMarkup(row_width=2)
     btn_1 = types.KeyboardButton('Да')
     btn_2 = types.KeyboardButton('Нет')
-    btn_3 = types.KeyboardButton('Посмотреть исходный код на сайте GitHub')
-    markup.add(btn_1, btn_2, btn_3)
-    bot.send_message(message.chat.id, 'Вы хотите продолжить', reply_markup=markup)
+    btn_website = types.KeyboardButton('Посмотреть исходный код на сайте GitHub')
+    markup.add(btn_1, btn_2, btn_website)
+    bot.send_message(chat_id, 'Вы хотите продолжить?', reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: message.text.lower() == 'да')
 def yes_choice(message):
-    bot.send_message(message.chat.id, 'Введите максимальное число рандома')
+    bot.send_message(message.chat.id, 'Введите максимальное число для генерации случайного числа')
     bot.register_next_step_handler(message, func_log)
 
 
 def func_log(message):
     try:
         text_message = message.text
-        result = random.randint(0, int(text_message))
-        bot.send_message(message.chat.id, f'Число загаданно.')
-        bot.send_message(message.chat.id, 'Попробуй угадать число:')
+        result = random.randint(1, int(text_message))
+        bot.send_message(message.chat.id, f'Число загадано.')
+        bot.send_message(message.chat.id, 'Попробуйте угадать число:')
         bot.register_next_step_handler(message, call, result)
     except ValueError:
-        bot.send_message(message.chat.id, 'Неверный формат ввода. Пожалуйста повторите попытку')
+        bot.send_message(message.chat.id, 'Неверный формат ввода. Пожалуйста, повторите попытку.')
         bot.register_next_step_handler(message, func_log)
 
 
 def call(message, result):
     try:
         text_random = int(message.text)
-        result = message.chat.id
         if text_random > 0:
             if text_random == result:
-                bot.send_message(message.chat.id, f'Вы угадали число. Ваше загаданное число --> {result}')
+                bot.send_message(message.chat.id, f'Вы угадали число! Загаданное число: {result}')
             elif text_random > result:
-                bot.send_message(message.chat.id, 'Загаданное число меньше <-- :)')
+                bot.send_message(message.chat.id, 'Загаданное число меньше.')
                 bot.register_next_step_handler(message, call, result)
             else:
-                bot.send_message(message.chat.id, 'Загаданное число больше --> :)')
+                bot.send_message(message.chat.id, 'Загаданное число больше.')
                 bot.register_next_step_handler(message, call, result)
         else:
-            bot.send_message(message.chat.id, 'Число не должно быть меньше нуля. Пожалуйста повторите попытку')
+            bot.send_message(message.chat.id, 'Число должно быть больше нуля. Пожалуйста, повторите попытку.')
             bot.register_next_step_handler(message, call, result)
     except ValueError:
-        bot.send_message(message.chat.id, 'Неверный формат ввода. Пожалуйста введите целое число')
+        bot.send_message(message.chat.id, 'Неверный формат ввода. Пожалуйста, введите целое число.')
         bot.register_next_step_handler(message, call, result)
 
 
 @bot.message_handler(func=lambda message: message.text.lower() == 'нет')
 def message_quiz(message):
-    bot.send_message(message.chat.id, 'Плохой выбор')
-    bot.send_message(message.chat.id, 'Играем в викторину')
+    bot.send_message(message.chat.id, 'Плохой выбор.')
+    bot.send_message(message.chat.id, 'Играем в викторину.')
     message_func_quiz(message)
 
 
@@ -73,7 +78,7 @@ def message_func_quiz(message):
     btn_3 = types.InlineKeyboardButton('Марс', callback_data='quiz1_Марс')
     btn_4 = types.InlineKeyboardButton('Земля', callback_data='quiz1_Земля')
     markup.add(btn_1, btn_2, btn_3, btn_4)
-    bot.send_message(message.chat.id, "Какая планета самая ближняя к Солнцу?", reply_markup=markup)
+    bot.send_message(message.chat.id, "Какая планета ближе всего к Солнцу?", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('quiz1_'))
@@ -82,9 +87,8 @@ def handle_quiz_answer(call):
     answer = call.data.split('_')[1]
     if answer == 'Меркурий':
         bot.send_message(chat_id,
-                         'Правильно! --> Меркурий — это самая близкая к Солнцу планета нашей Солнечной системы. Он расположен на среднем расстоянии около 58 миллионов километров (36 миллионов миль) от Солнца. Из-за своей близости к Солнцу, Меркурий имеет очень высокие дневные температуры и очень низкие ночные температуры. Планета совершает полный оборот вокруг Солнца примерно за 88 земных дней.')
-
-        Total_PROGRESS_of_RESPONCES[chat_id] += 1
+                         'Правильно! Меркурий — самая близкая к Солнцу планета нашей Солнечной системы. Она расположена на среднем расстоянии около 58 миллионов километров от Солнца. Из-за своей близости к Солнцу, Меркурий имеет очень высокие дневные температуры и очень низкие ночные температуры. Планета совершает полный оборот вокруг Солнца примерно за 88 земных дней.')
+        Total_PROGRESS_of_RESPONSES[chat_id] += 1
         ask_next_question(call.message)
     else:
         bot.send_message(chat_id, 'Неправильно, правильный ответ: Меркурий')
@@ -108,8 +112,8 @@ def handle_next_question(call):
     answer = call.data.split('_')[1]
     if answer == 'Гипотенуза':
         bot.send_message(chat_id,
-                         'Правильно! Гипотенуза - это самая длинная сторона прямоугольного треугольника.')
-        Total_PROGRESS_of_RESPONCES[chat_id] += 1
+                         'Правильно! Гипотенуза — самая длинная сторона прямоугольного треугольника.')
+        Total_PROGRESS_of_RESPONSES[chat_id] += 1
     else:
         correct_answer = 'Гипотенуза'
         bot.send_message(chat_id, f'Неправильно, правильный ответ: {correct_answer}')
@@ -133,8 +137,8 @@ def handler_next_question3(call):
     answer = call.data.split('_')[1]
     if answer == 'Моль':
         bot.send_message(chat_id,
-                         'Молодец! Это правильный ответ.\nПояснение: --> Моль — это единица измерения количества вещества в Международной системе единиц (СИ). Один моль содержит ровно столько атомов, молекул или других частиц, сколько содержится в 12 граммах углерода-12, что составляет примерно 6.022 x 10^23 частиц (число Авогадро).')
-        Total_PROGRESS_of_RESPONCES[chat_id] += 1
+                         'Молодец! Это правильный ответ.\nПояснение: Моль — единица измерения количества вещества в Международной системе единиц (СИ). Один моль содержит ровно столько атомов, молекул или других частиц, сколько содержится в 12 граммах углерода-12, что составляет примерно 6.022 x 10^23 частиц (число Авогадро).')
+        Total_PROGRESS_of_RESPONSES[chat_id] += 1
     else:
         correct_answer = 'Моль'
         bot.send_message(chat_id, f'Ответ неверный. Правильный ответ: {correct_answer}')
@@ -160,7 +164,7 @@ def handler_next_question4(call):
     if answer == 'Радиус':
         bot.send_message(chat_id,
                          'Правильно! Радиус — это отрезок, соединяющий центр окружности с любой точкой на окружности.')
-        Total_PROGRESS_of_RESPONCES[chat_id] += 1
+        Total_PROGRESS_of_RESPONSES[chat_id] += 1
     else:
         correct_answer = 'Радиус'
         bot.send_message(chat_id, f'Ответ неверный. Правильный ответ: {correct_answer}')
@@ -186,7 +190,7 @@ def handler_next_question5(call):
     if answer == 'Линейная':
         bot.send_message(chat_id,
                          'Правильно! Линейная функция представляет собой прямую линию на графике и описывает пропорциональную зависимость между переменными.')
-        Total_PROGRESS_of_RESPONCES[chat_id] += 1
+        Total_PROGRESS_of_RESPONSES[chat_id] += 1
     else:
         correct_answer = 'Линейная'
         bot.send_message(chat_id, f'Ответ неверный. Правильный ответ: {correct_answer}')
@@ -212,15 +216,35 @@ def handler_next_question6(call):
     if answer == 'Атом':
         bot.send_message(chat_id,
                          'Правильно! Атом — это наименьшая частица химического элемента, которая сохраняет его химические свойства.')
-        Total_PROGRESS_of_RESPONCES[chat_id] += 1
+        Total_PROGRESS_of_RESPONSES[chat_id] += 1
     else:
         correct_answer = 'Атом'
         bot.send_message(chat_id, f'Ответ неверный. Правильный ответ: {correct_answer}')
-    Value_results(call.message)
+    value_results(call.message)
 
 
-def Value_results(message):
+def value_results(message):
     chat_id = message.chat.id
-    score = Total_PROGRESS_of_RESPONCES[chat_id]
+    score = Total_PROGRESS_of_RESPONSES[chat_id]
     bot.send_message(chat_id, f'Ваш результат: {score}/6')
+    bot.send_message(chat_id, 'Вы желаете продолжить?')
 
+
+@bot.message_handler(func=lambda message: message.text.strip() == "Посмотреть исходный код на сайте GitHub")
+def message_browser(message):
+    markup = types.InlineKeyboardMarkup()
+    url = 'https://github.com/GhOstKroMshtEiN/Telegram_bot_quiz/blob/main/Telegram_bot_quiz.py'
+    btn_url = types.InlineKeyboardButton('Перейти на GitHub', url=url)
+    markup.add(btn_url)
+
+    bot.send_message(message.chat.id, 'Нажмите кнопку ниже, чтобы перейти на сайт GitHub в мой репозиторий:',
+                     reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: True)
+def all_messages(message):
+    message.text = "Команды для управления ботом:\n/start - начать сначала.\n/start_game - начать игру."
+    bot.reply_to(message, f'Пожалуйста, сделайте выбор из\n {message.text}')
+
+
+bot.polling(non_stop=True)
